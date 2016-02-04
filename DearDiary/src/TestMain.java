84c4503e623cdd9c072a144561b9c7ff7a6c1fdf
Leogaogithub <longhaogao@gmail.com>
2016-02-04 16:47:24 -0600

    Update TestMain.java

--- DearDiary/src/TestMain.java
+++ DearDiary/src/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, this is first test");
+		System.out.println("this command is modifyed from web browser");
 	}
 
 }
